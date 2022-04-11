f55798c74df968c824e55313b0926d9a6b1f674a
Waffle <88282807+WaffleKone@users.noreply.github.com>
2022-04-11 19:10:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
 class Casino {
-    // Write code here
+    constructor(name, fakeCoin){
+        this.name = name;
+        this.timesPlayed = 0;
+        this.fakeCoin = fakeCoin
+    }
+    playGame(bet) {
+        if (Math.random() <= 0.5 || this.fakeCoin === true) {
+            console.log(`${this.name} wins the ${bet}!`)
+            this.timesPlayed += 1
+        } else {
+            console.log(`You win the ${bet * (this.timesPlayed+1)}!`)
+            this.timesPlayed += 1
+        }
+    }
+    rollDie(sides) {
+        console.log( 1 + Math.floor(Math.random()*sides))
+    }
 };
 
 // TESTS
-const myCasino = new Casino("HackerU Casino");
-console.log(myCasino);
-myCasino.playGame(5);
-myCasino.playGame(15);
-myCasino.playGame(25);
-myCasino.playGame(35);
+// const myCasino = new Casino("HackerU Casino", false);
+// console.log(myCasino);
+// myCasino.playGame(5);
+// myCasino.playGame(15);
+// myCasino.playGame(25);
+// myCasino.playGame(35);
 
 // BONUS TESTS
-/*
-const myBonusCasino = new Casino("HackerU Bonus Casino", true);
-console.log(myBonusCasino);
-myBonusCasino.playGame(5);
-myBonusCasino.playGame(15);
-myBonusCasino.playGame(25);
-myBonusCasino.playGame(35);
-*/
+
+// const myBonusCasino = new Casino("HackerU Bonus Casino", true);
+// console.log(myBonusCasino);
+// myBonusCasino.playGame(5);
+// myBonusCasino.playGame(15);
+// myBonusCasino.playGame(25);
+// myBonusCasino.playGame(35);
 
 // Extra BONUS TESTS
-/*
+
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
-*/
+
